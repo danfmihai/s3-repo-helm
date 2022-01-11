0b43838a1b1b2b1ae1228f3c603a72c0eba27b5b
Mihai <mihai@pop-os.localdomain>
2022-01-11 18:07:09 -0500

    added helm repo resource

--- part1/main.tf
+++ part1/main.tf
@@ -1,3 +1,4 @@
+# Create a bucket in s3 with the environment variable TF_VAR_bucket_name. Make sure you export this value!
 module "s3" {
   source        = "./s3"
   acl_value     = var.acl_value
@@ -5,3 +6,26 @@ module "s3" {
   versioning    = var.versioning
   force_destroy = var.force_destroy
 }
+
+#create folder to host helm resository
+resource "aws_s3_bucket_object" "helm_repo_folder" {
+  depends_on = [
+    module.s3
+  ]
+  bucket = var.bucket_name
+  key    = "${var.folder_to_upload}/"
+}
+
+# init the folder as Helm Repository
+resource "null_resource" "init_helm" {
+   depends_on = [
+     aws_s3_bucket_object.helm_repo_folder
+   ]
+  # make sure you have installed the 
+  # init the s3 bucket as a helm s3 plugin
+  # To install the helm-s3 plugin on your client machine, run the following command: helm plugin install https://github.com/hypnoglow/helm-s3.git
+  provisioner "local-exec" {
+    command = "helm s3 init s3://${var.bucket_name}/${var.folder_to_upload}"
+  }
+}
+
