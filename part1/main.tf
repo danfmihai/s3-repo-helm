c6f9e742891aed99578fa91fd756d0ee7de92524
Mihai <mihai@pop-os.localdomain>
2022-01-12 12:14:10 -0500

    added interpreter

--- part1/main.tf
+++ part1/main.tf
@@ -8,7 +8,7 @@ module "s3" {
 }
 
 locals {
-  folder_name = trim(aws_s3_bucket_object.helm_repo_folder.id,"/")
+  folder_name = trim(aws_s3_bucket_object.helm_repo_folder.id, "/")
 }
 
 #create folder to host helm resository
@@ -22,30 +22,27 @@ resource "aws_s3_bucket_object" "helm_repo_folder" {
 
 # init the folder as Helm Repository
 resource "null_resource" "init_helm" {
-   depends_on = [
-     aws_s3_bucket_object.helm_repo_folder
-   ]
+  depends_on = [
+    aws_s3_bucket_object.helm_repo_folder
+  ]
   #checks if plugin exists and if not it will install it
   provisioner "local-exec" {
     command = " if [ -z $(helm plugin list | grep s3 | awk '{print $1}') ]; then  helm plugin install https://github.com/hypnoglow/helm-s3.git; fi"
   }
-  
+
   # To install the helm-s3 plugin on your client machine, run the following command: helm plugin install https://github.com/hypnoglow/helm-s3.git
   provisioner "local-exec" {
     command = <<EOT
     helm s3 init s3://${var.bucket_name}/${var.folder_to_upload}
-    helm repo add ${local.folder_name} s3://${module.s3.id}/${local.folder_name}/
+    if [ -z $(helm repo list | grep sample | awk '{print $1}') ]; then 
+      helm repo add ${local.folder_name} s3://${module.s3.id}/${local.folder_name}/
+    fi  
     EOT
   }
   # push a sample chart into s3 repository
   provisioner "local-exec" {
-    command = <<EOT
-    if [ -f "${var.chart_name}" ]; then 
-      helm s3 push ${var.chart_name} ${local.folder_name} 
-    else 
-      echo "File doen't exist!" 
-    fi
-    EOT
+    interpreter = ["/bin/bash", "-c"]
+    command     = " if [ -f ${var.chart_name} ]; then helm s3 push ${var.chart_name} ${local.folder_name}; fi"
   }
 
 }
